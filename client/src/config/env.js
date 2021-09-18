1ca989c4e76dbd77ef779729bfce25e92093792f
Aseer Ishraqul Hoque <aseerishraque@gmail.com>
2021-09-19 02:06:43 +0600

    vgy -v0.1

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -2,6 +2,6 @@ class env {
     static baseURL = "http://localhost:8000";
 
     static CLIENT_ID = 2;
-    static CLIENT_SECRET = "r2lnpiA8jB4J2OXebr55RFYNkIjfF9LlRLbjtWJn";
+    static CLIENT_SECRET = "aQqLoH9DM4TNRJPw75W10uDccI1sRFGHwkUyPYYp";
 }
 export default env;
