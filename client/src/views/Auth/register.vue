0138f45fd6f75662b7c903cd66fd434e71817048
Aseer Ishraqul Hoque <aseerishraque@gmail.com>
2021-09-28 02:58:40 +0600

    reg form updatev1

--- client/src/views/Auth/register.vue
+++ client/src/views/Auth/register.vue
@@ -11,9 +11,18 @@
                     <button type="submit" class="btn bg-black rounded-md mt-10 float-right">Back to Home Page</button> 
                 </router-link>
             <h1 class="clear-right mt-6 text-xl font-semibold text-black tracking-ringtighter sm:text-2xl title-font">Sign up to your account</h1>
-                <div v-if="statusText.length > 0" class="mt-5 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <!-- <div v-if="statusText.length > 0" class="mt-5 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                     <span class="block sm:inline">{{ statusText }}</span>
-                </div>
+                </div> -->
+                <ul v-if="statusText.length > 0" class="mt-1 ml-2 list-disc list-inside bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+                <li v-for="(allErrors,idx) in errors" :key="idx" v-show="allErrors.length > 0">
+                    <span v-for="errorKeys in allErrors" :key="errorKeys">
+                        <span v-for="errorMessage in errorKeys" :key="errorMessage">
+                            {{ errorMessage }}
+                        </span> 
+                    </span>
+                </li>
+            </ul>
                 <form enctype="multipart/form-data" class="mt-6 flex flex-col gap-4" method="POST" @submit.prevent="register(user)">
                     <div class="flex flex-col md:flex-row gap-4 justify-center">
                         <div class="w-full">
@@ -24,9 +33,9 @@
                             <label class="mb-1 block text-sm font-medium leading-relaxed tracking-tighter text-gray-700">Email Address</label>
                             <input type="email" v-model="user.email" required placeholder="Your Email" class="w-full px-4 py-2 text-base text-black transition duration-300 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:ring-2 ring-offset-2">
                         </div>
-                        <div v-if="errors.email.length > 0" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                        <!-- <div v-if="errors.email.length > 0" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                             <span class="block sm:inline">{{ errors.email[0]}}</span>
-                        </div>
+                        </div> -->
                         
                     </div>
                     <div class="flex flex-col md:flex-row gap-4 justify-center">
@@ -35,7 +44,7 @@
                             <input type="text" v-model="user.mobile_no" required minlength="11" placeholder="Your Mobile No." class="w-full px-4 py-2 text-base text-black transition duration-300 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:ring-2 ring-offset-2">
                         </div> 
                     </div>
-                        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert" v-show="user.mobile_no.length>11">
+                        <!-- <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert" v-show="user.mobile_no.length>11">
                             <span class="block sm:inline">Must be 11 digit and Bangladeshi number</span>
                         </div>
                  
@@ -44,7 +53,7 @@
                         </div>
                         <div v-if="errors.mobile_no.length > 0" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                             <span class="block sm:inline">{{ errors.mobile_no[0]}}</span>
-                        </div>
+                        </div> -->
                     <div>
                         <label class="mb-1 block text-sm font-medium leading-relaxed tracking-tighter text-gray-700">Password</label>
                         <input type="password" v-model="user.password" required minlength="6" placeholder="Your Password" class="w-full px-4 py-2 text-base text-black transition duration-300 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:ring-2 ring-offset-2" >
@@ -53,12 +62,12 @@
                         <label class="mb-1 block text-sm font-medium leading-relaxed tracking-tighter text-gray-700">Confirm Password</label>
                         <input type="password" v-model="user.password_confirmation" required minlength="6" placeholder="Confirm Password" class="w-full px-4 py-2 text-base text-black transition duration-300 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:ring-2 ring-offset-2" >
                     </div>
-                    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert" v-show="user.password!=user.password_confirmation && user.password_confirmation.length>0">
-                        <span class="block sm:inline">Password and Confirm Password doesnt match</span>
-                    </div>
-                    <div v-if="errors.password.length > 0" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                            <span class="block sm:inline">{{ errors.password[0]}}</span>
+                        <!-- <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert" v-show="user.password!=user.password_confirmation && user.password_confirmation.length>0">
+                            <span class="block sm:inline">Password and Confirm Password doesnt match</span>
                         </div>
+                        <div v-if="errors.password.length > 0" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                            <span class="block sm:inline">{{ errors.password[0]}}</span>
+                        </div> -->
                     <div class="flex flex-col md:flex-row gap-4 justify-center">
                         <div class="w-full">
                                 <label class="mb-1 block text-sm font-medium leading-relaxed tracking-tighter text-gray-700">Account Type</label>
@@ -78,7 +87,7 @@
                         <label class="mb-1 block text-sm font-medium leading-relaxed tracking-tighter text-gray-700">NID Number(For institution Head of the institution)</label>
                         <input type="number" v-model="user.nid_no" required minlength="10" placeholder="Your NID Number" class="w-full px-4 py-2 text-base text-black transition duration-300 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:ring-2 ring-offset-2" >
                     </div> 
-                        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert" v-show="user.nid_no.length>10">
+                        <!-- <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert" v-show="user.nid_no.length>10">
                             <span class="block sm:inline">NID Number Must be 10 digit</span>
                         </div>
                  
@@ -87,7 +96,7 @@
                         </div>
                         <div v-if="errors.nid_no.length > 0" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                             <span class="block sm:inline">{{ errors.nid_no[0]}}</span>
-                        </div>
+                        </div> -->
                     <div>
                         <label class="mb-1 block text-sm font-medium leading-relaxed tracking-tighter text-gray-700">NID Scan copy(Front Part)</label>
                         <input type="file" v-on:change="onImageChange" placeholder="NID Scan copy(front Part)" class="w-full px-4 py-2 text-base text-black transition duration-300 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:ring-2 ring-offset-2" >
@@ -97,17 +106,17 @@
                         <input type="file" v-on:change="onImageChange1" placeholder="NID Scan copy(back Part)" class="w-full px-4 py-2 text-base text-black transition duration-300 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:ring-2 ring-offset-2" >
                     </div>
                     <div  v-if="loginType">
-                        <div>
-                            <label class="mb-1 block text-sm font-medium leading-relaxed tracking-tighter text-gray-700">VAT Number</label>
-                            <input :disabled="!loginType" type="number" v-model="user.vat_no" placeholder="Your VAT Number"  class="w-full px-4 py-2 text-base text-black transition duration-300 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:ring-2 ring-offset-2" >
-                        </div> 
-                        <div v-if="errors.vat_no.length > 0" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <div>
+                        <label class="mb-1 block text-sm font-medium leading-relaxed tracking-tighter text-gray-700">VAT Number</label>
+                        <input :disabled="!loginType" type="number" v-model="user.vat_no" placeholder="Your VAT Number"  class="w-full px-4 py-2 text-base text-black transition duration-300 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:ring-2 ring-offset-2" >
+                    </div> 
+                        <!-- <div v-if="errors.vat_no.length > 0" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                             <span class="block sm:inline">{{ errors.vat_no[0]}}</span>
-                        </div>
-                        <div>
-                            <label class="mb-1 block text-sm font-medium leading-relaxed tracking-tighter text-gray-700">VAT Scan Copy</label>
-                            <input :disabled="!loginType" type="file" v-on:change="onImageChange2" placeholder="Scan copy" class="w-full px-4 py-2 text-base text-black transition duration-300 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:ring-2 ring-offset-2" >
-                        </div> 
+                        </div> -->
+                    <div>
+                        <label class="mb-1 block text-sm font-medium leading-relaxed tracking-tighter text-gray-700">VAT Scan Copy</label>
+                        <input :disabled="!loginType" type="file" v-on:change="onImageChange2" placeholder="Scan copy" class="w-full px-4 py-2 text-base text-black transition duration-300 ease-in-out transform border-transparent rounded-lg bg-gray-100 focus:border-gray-500 focus:bg-white focus:outline-none focus:ring-2 ring-offset-2" >
+                    </div> 
                     </div>                    
                     <button type="submit" class="block w-full px-4 py-3  mt-6 mb-4 font-semibold text-white transition duration-300 ease-in-out transform bg-black rounded-lg hover:bg-gray-700 active:bg-black focus:shadow-outline focus:outline-none focus:ring-2 focus:ring-black ring-offset-2">
                         Register
@@ -227,6 +236,7 @@ export default {
                     router.push({ name: 'auth.login'});
                 })
                 .catch(error => {
+                   
                     this.statusText = error.response.statusText
 					let data = error.response.data
                     for (let key in data.errors) {
@@ -235,7 +245,8 @@ export default {
 						if (errorMessage){
 							this.errors[key] = errorMessage
 						}
-					}
+					} 
+                    console.log(this.errors);
 					
 				})
             }
